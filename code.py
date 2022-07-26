a74544e9698e73b0df0e1e88abe28b1a09794a1d
DenisDeFaria <74181819+DenisDeFaria@users.noreply.github.com>
2022-07-26 15:02:09 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,9 +1,9 @@
 #Tu lances le programmme 
 
 
-nb_issue=input("Nombre d'issues possible : ") # nombre de paris sur le match de possible pour couvrir toutes les options   
-mise_max=input("Mise maximale possible : ")   # Les côtes boostées en général on une mise maximale donc c'est ce qu'il faut renseigner 
-issue_max=input("issue de la mise max : ")     # Tu dois donner le numéro de la côte de la côte boosté
+#nb_issue=input("Nombre d'issues possible : ") # nombre de paris sur le match de possible pour couvrir toutes les options   
+#mise_max=input("Mise maximale possible : ")   # Les côtes boostées en général on une mise maximale donc c'est ce qu'il faut renseigner 
+#issue_max=input("issue de la mise max : ")     # Tu dois donner le numéro de la côte de la côte boosté
 
 # Après tu vas devoir renseigner les différentes côtes donc la tu choisis en fonction des différents sites de paris afin d'avoir la meilleur côte possible, donc faut vraiment que tu sois sur quasi tous les sites pour optimiser le truc, je sais que winamax fait beaucoup de côte boostés donc check souvent chez eux
 # Si il te dit pas de surebet ça veut dire que t'as pas de côte assez avantageuses pour t'assurer de faire de l'argent 
@@ -13,23 +13,21 @@ issue_max=input("issue de la mise max : ")     # Tu dois donner le numéro de la
 # Dernier truc fait gaffe les côtes bouge souvent donc juste avant de faire ton paris vérifie bien les côtes sinon ca peut faire que t'es pas forcément gagnant
 
 
-nb_issue=int(nb_issue)
-mise_max=int(mise_max)
-issue_max=int(issue_max)
+
 
 def cotes(nb_issue):
     Cote=[]
     for k in range(nb_issue):
-        c=input("Cotes de l'issue "+str(k+1)+" : ")
+        c=input("Côtes de l'issue "+str(k+1)+" : ")
         Cote.append(float(c))
     return(Cote)
 
-Cote = cotes(nb_issue)
+#Cote = cotes(nb_issue)
 
 def inverse(Cote):
-    
+    l=len(Cote)
     inv=0
-    for k in range(len(Cote)):
+    for k in range(l):
         inv=1/Cote[k]+inv
     if inv<1:
         return(True)
@@ -54,14 +52,18 @@ def mise2(Cote,mise_max,issue_max):
             P.append(T/(Cote[k]*inverse2(Cote)))
     return(P)
 
-P=mise2(Cote,mise_max,issue_max)
+#P=mise2(Cote,mise_max,issue_max)
 
-def fin(nb_issue,P):
+def fin(nb_issue, mise_max, issue_max):
+    Cote = cotes(nb_issue)
+    P=mise2(Cote,mise_max,issue_max)
+
+
     a="\n \n"
     S=0
     for j in range(len(P)):
         S=S+P[j]
-    if inverse(nb_issue)==False:
+    if inverse(Cote)==False:
         print("Pas de Surebet")
         print(str(1+1-inverse2(Cote)))
     else:
@@ -75,5 +77,15 @@ def fin(nb_issue,P):
 
         print(a)
 
+if __name__ == '__main__':
+    nb_issue=input("Nombre d'issues possible : ") # nombre de paris sur le match de possible pour couvrir toutes les options   
+    nb_issue=int(nb_issue)
 
-fin(nb_issue,P)
+    mise_max=input("Mise maximale possible : ")   # Les côtes boostées en général on une mise maximale donc c'est ce qu'il faut renseigner 
+    mise_max=int(mise_max)
+
+    issue_max=input("issue de la mise max : ")     # Tu dois donner le numéro de la côte de la côte boosté
+    issue_max=int(issue_max)
+
+
+    fin(nb_issue, mise_max, issue_max)
